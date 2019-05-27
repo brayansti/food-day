ca36b59f1c1a2d8d39ef8ccff2a8d56b736dd123
meca1 <drramosdev@gmail.com>
2019-05-26 23:29:58 -0500

    create service to graphcool with apollo and graphQL

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,28 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+//import { GraphQLModule } from './graphql.module';
+import { HttpClientModule } from '@angular/common/http';
 
+//Apollo things
+
+import { Apollo } from 'apollo-angular';
+import { ApolloModule } from 'apollo-angular';
+import {HttpLinkModule, HttpLink} from 'apollo-angular-link-http';
+import {InMemoryCache} from 'apollo-cache-inmemory';
+
+const uri ="https://api.graph.cool/simple/v1/cjw5q6vt122450164qnmjxk3o"
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    HttpLinkModule,
+    ApolloModule,
+    HttpClientModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
@@ -20,4 +37,14 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+  constructor(
+    apollo: Apollo,
+    httpLink: HttpLink
+  ) { 
+    apollo.create({
+      link: httpLink.create({ uri }), 
+      cache: new InMemoryCache()
+    }); 
+  }
+}

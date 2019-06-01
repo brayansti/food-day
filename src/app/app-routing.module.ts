4404d450f07907ba0282acee837f4707d9a256ca
Brayan Camargo <brayan440@hotmail.com>
2019-05-31 22:00:42 -0500

    Before angular Material

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ const routes: Routes = [
   { path: 'home', loadChildren: './pages/home/home.module#HomePageModule' },
   { path: 'register', loadChildren: './pages/register/register.module#RegisterPageModule' },
   { path: 'initial', loadChildren: './pages/initial/initial.module#InitialPageModule' },
+  { path: 'form-borrower', loadChildren: './pages/form-borrower/form-borrower.module#FormBorrowerPageModule' },
 
 ];
 

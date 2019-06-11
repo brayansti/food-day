13b2115d1850d874e7140203e2790b0622d000dd
Brayan Camargo <brayansti@gmail.com>
2019-06-10 21:30:55 -0500

    Solved Button submit

--- src/app/pages/initial/initial.page.ts
+++ src/app/pages/initial/initial.page.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 
+import { NavController, ModalController } from '@ionic/angular';
+import { AuthenticationService } from '../../services/authentication.service';
+
+
 @Component({
   selector: 'app-initial',
   templateUrl: './initial.page.html',
@@ -8,12 +12,22 @@ import { AlertController } from '@ionic/angular';
 })
 
 export class InitialPage implements OnInit {
+  userEmail: string;
 
-  constructor(public alertController: AlertController) {
+  constructor(
+    public alertController: AlertController,
+    private navCtrl: NavController,
+    private authService: AuthenticationService
+    ) {
 
   }
   ngOnInit() {
     this.presentAlert()
+    if (this.authService.userDetails()) {
+      this.userEmail = this.authService.userDetails().email;
+    } else {
+      this.navCtrl.navigateBack('/home');
+    }
   }
   async presentAlert() {
     const alert = await this.alertController.create({
@@ -25,5 +39,16 @@ export class InitialPage implements OnInit {
 
     await alert.present();
   }
+
+    logout() {
+      this.authService.logoutUser()
+        .then(res => {
+          console.log(res);
+          this.navCtrl.navigateBack('');
+        })
+        .catch(error => {
+          console.log(error);
+        })
+    }
 
 }

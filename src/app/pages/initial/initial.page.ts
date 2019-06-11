4117f41629a5dcb9e84ec902961b78847ab20607
Brayan Camargo <brayansti@gmail.com>
2019-06-10 20:38:06 -0500

    Home avances

--- src/app/pages/initial/initial.page.ts
+++ src/app/pages/initial/initial.page.ts
@@ -1,49 +1,29 @@
-  import { Component, OnInit } from '@angular/core';
-  import { AlertController } from '@ionic/angular';
-  import { NavController, ModalController } from '@ionic/angular';
-  import { AuthenticationService } from '../../services/authentication.service';
+import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
-  @Component({
-    selector: 'app-initial',
-    templateUrl: './initial.page.html',
-    styleUrls: ['./initial.page.scss'],
-  })
+@Component({
+  selector: 'app-initial',
+  templateUrl: './initial.page.html',
+  styleUrls: ['./initial.page.scss'],
+})
 
-  export class InitialPage implements OnInit {
-    userEmail: string;
+export class InitialPage implements OnInit {
 
-    constructor(
-      public alertController: AlertController,
-      private navCtrl: NavController,
-      private authService: AuthenticationService
-    ) { }
+  constructor(public alertController: AlertController) {
 
-    ngOnInit() {
-      this.presentAlert()
-      if (this.authService.userDetails()) {
-        this.userEmail = this.authService.userDetails().email;
-      } else {
-        this.navCtrl.navigateBack('/home');
-      }
-    }
-    async presentAlert() {
-      const alert = await this.alertController.create({
-        header: 'Bienvenido',
-        message: 'Seleciona el rol que se ajuste a tus intereses.',
-        buttons: ['LISTO']
-      });
-
-      await alert.present();
-    }
-
-    logout() {
-      this.authService.logoutUser()
-        .then(res => {
-          console.log(res);
-          this.navCtrl.navigateBack('');
-        })
-        .catch(error => {
-          console.log(error);
-        })
-    }
   }
+  ngOnInit() {
+    this.presentAlert()
+  }
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Bienvenido',
+      message: 'Seleciona el rol que se ajuste a tus intereses.',
+      buttons: ['LISTO'],
+      cssClass: 'alertDanger'
+    });
+
+    await alert.present();
+  }
+
+}

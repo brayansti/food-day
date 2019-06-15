fff3faa46f70cbb69a7ff276579121ad03dcee22
Brayan Camargo <brayansti@gmail.com>
2019-06-15 16:03:17 -0500

    Profile

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +7,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
-
-  constructor() { }
+  
+  constructor(public alertController: AlertController) {}
 
   ngOnInit() {
+    // this.presentAlert( 1, '$300.000');
+  }
+
+
+    async presentAlert( state:number, value:string  ) {
+      let txtNoAproved = 'No ha sido posible aprobar tu crédito, puedes solicitar un valor diferente e Intentar de nuevo.';
+      let txtAproved = `Tu crédito por ${value} ha sido aprobado. Pronto nos comunicaremos contigo para mas detalles.`;
+      let txtMessage = state == 0 ? 'Lo sentimos' : '¡Felicidades!';
+      let txtShow = state == 0 ? txtNoAproved : txtAproved;
+      const alert = await this.alertController.create({
+        // header: 'Confirm!',
+        // subHeader: 'Subtitle', 
+        message: `
+          <div class="containerAlert containerAlert--congratulation">
+            <ion-img src="../../../assets/images/alert/aproved.jpg"></ion-img>
+            <div class="containerAlert__message">
+              <ion-text>
+                <h4>${txtMessage}</h4>
+              </ion-text>
+              <ion-text> ${txtShow} </ion-text>
+            </div>
+          </div>
+        `,
+        buttons: [
+          // {
+          //   text: 'Cancel',
+          //   role: 'cancel',
+          //   cssClass: 'secondary',
+          //   handler: (blah) => {
+          //     console.log('Confirm Cancel: blah');
+          //   }
+          // },
+          {
+            text: 'ENTENDIDO',
+            handler: () => {
+              console.log('Confirm Okay');
+            }
+          }
+        ]
+      });
+
+    await alert.present();
   }
 
 }

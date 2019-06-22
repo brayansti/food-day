ea3ff52f998ff50d0bd51ced04eb55e0ac51bab7
Brayan Camargo <brayansti@gmail.com>
2019-06-22 16:31:47 -0500

    Profile service

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 import { AlertController } from '@ionic/angular';
+
+
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
+import { ApolloQueryResult } from 'apollo-client';
+import { AuthenticationService } from '../../services/firebase/authentication.service';
 
 @Component({
   selector: 'app-profile',
@@ -9,15 +16,13 @@ import { AlertController } from '@ionic/angular';
 export class ProfilePage implements OnInit {
   
   constructor(
-    public alertController: AlertController
+    public alertController: AlertController,
+    private apollo: Apollo,
+    private navCtrl: NavController,
   ) {}
 
-  ngOnInit() {
-    // this.presentAlert( 1, '$300.000');
-  }
 
-
-    async presentAlert( state:number, value:string  ) {
+  async presentAlert( state:number, value:string  ) {
       let txtNoAproved = 'No ha sido posible aprobar tu crédito, puedes solicitar un valor diferente e Intentar de nuevo.';
       let txtAproved = `Tu crédito por ${value} ha sido aprobado. Pronto nos comunicaremos contigo para mas detalles.`;
       let txtMessage = state == 0 ? 'Lo sentimos' : '¡Felicidades!';
@@ -53,8 +58,42 @@ export class ProfilePage implements OnInit {
           }
         ]
       });
+      await alert.present();
+  }
 
-    await alert.present();
+  rates: any;
+  loading = true;
+  error: any;
+
+  pushUserNavCtrl() {
+    // this.navCtrl.navigateForward(`/user/${ this.valor }`);
+    this.navCtrl.navigateForward(`/mapa`);
+  }
+
+  ngOnInit() {
+    this.apollo
+      .watchQuery({
+        query: gql`
+            {
+              User(
+                id : "cjx7szta0099s0176ihmzw9f5"
+              ){
+                name
+                addresses{
+                  addressDetail1
+                  addressDetail2
+                  address
+                }
+              }
+            }
+        `,
+      })
+      .valueChanges.subscribe((result: ApolloQueryResult<any>) => {
+        this.rates = result.data && result.data.User;
+        this.loading = result.loading;
+        this.error = result.errors;
+        console.log( this.rates );
+      });
   }
 
 }
